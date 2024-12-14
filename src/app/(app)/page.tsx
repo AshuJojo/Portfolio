62f3b6f2c5c905cd3f5bfd1f0a1e4d0dbb59a105
AshuJojo <ashutoshsharma.120599@gmail.com>
2024-12-14 21:09:00 +0530

    Shadcn configured with custom colors

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,4 +1,9 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <div>Portfolio</div>;
+  return (
+    <div>
+      <Button variant={"default"}>Portfolio</Button>
+    </div>
+  );
 }

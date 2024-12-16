adc45736962a99e2a699f394ca9fedec7b980fc0
AshuJojo <ashutoshsharma.120599@gmail.com>
2024-12-16 20:15:48 +0530

    Added Skills Section

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/sections/HeroSection";
+import SkillsSection from "@/components/sections/SkillsSection";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Navbar />
       <main>
         <HeroSection />
+        <SkillsSection />
       </main>
     </>
   );

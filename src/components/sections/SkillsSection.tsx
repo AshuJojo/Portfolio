647a5111e54d7fda3f2406cc083bad1e10cd2c36
AshuJojo <ashutoshsharma.120599@gmail.com>
2024-12-27 22:20:56 +0530

    Filter SkillCategories tag working

--- src/components/sections/SkillsSection.tsx
+++ src/components/sections/SkillsSection.tsx
@@ -2,13 +2,16 @@
 
 import useFetch from "@/hooks/useFetch";
 import { Skill, SkillCategory } from "@/payload-types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SkillCard from "../SkillCard";
 import SkillCategoryTabs from "../SkillCategoryTabs";
 import { Card } from "../ui/card";
 import { Skeleton } from "../ui/skeleton";
 
 const SkillsSection = () => {
+  const [curCategory, setCurCategory] = useState<string>("");
+  const [filteredSkills, setFilteredSkills] = useState<Skill[]>([]);
+
   const {
     data: skillCategories,
     error: skillCategoriesError,
@@ -21,11 +24,27 @@ const SkillsSection = () => {
     loading: skillsLoading,
   } = useFetch<Skill>("/api/skills");
 
-  const [curCategory, setCurCategory] = useState<string>("");
-
   const updateCurCategory = (categoryId: string) => {
     setCurCategory(categoryId);
   };
+
+  useEffect(() => {
+    if (skills && skills.length > 0) {
+      if (curCategory === "") {
+        setFilteredSkills(skills);
+      } else {
+        const filteredData = skills.filter((skill) => {
+          if (!skill.categories) return true;
+
+          return skill.categories.find(
+            (category) => typeof category !== "string" && category.id === curCategory
+          );
+        });
+
+        setFilteredSkills(filteredData);
+      }
+    }
+  }, [skills, curCategory]);
 
   return (
     <div className="container min-h-screen border-b-4 border-black">
@@ -38,9 +57,9 @@ const SkillsSection = () => {
             {[...Array(5)].map((_, i) => (
               <div
                 key={i}
-                className="h-auto px-4 py-2 rounded-full bg-white border-2 border-black drop-shadow-2xl "
+                className="h-auto rounded-full border-2 border-black bg-white px-4 py-2 drop-shadow-2xl"
               >
-                <Skeleton className="h-4 w-12 bg-gray-300 my-auto" />
+                <Skeleton className="my-auto h-4 w-12 bg-gray-300" />
               </div>
             ))}
           </div>
@@ -53,7 +72,7 @@ const SkillsSection = () => {
           />
         )}
         {/* Skills */}
-        <div className="overflow-hidden grid flex-grow grid-cols-1 items-center justify-items-center gap-4 rounded-xl border-2 border-black bg-white p-6 drop-shadow-4xl sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6">
+        <div className="grid flex-grow grid-cols-1 justify-items-center gap-4 overflow-hidden rounded-xl border-2 border-black bg-white p-6 drop-shadow-4xl sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6">
           {(skillsLoading || skillsError) && (
             <>
               {[...Array(8)].map((_, i) => (
@@ -64,15 +83,17 @@ const SkillsSection = () => {
                   <div className="p-4">
                     <Skeleton className="my-auto h-24 w-auto bg-gray-300" />
                   </div>
-                  <div className="border-t-2 border-black bg-primary py-4 px-2">
+                  <div className="border-t-2 border-black bg-primary px-2 py-4">
                     <Skeleton className="my-auto h-4 w-auto bg-gray-300" />
                   </div>
                 </Card>
               ))}
             </>
           )}
-          {skills &&
-            skills.map((skill) => <SkillCard key={skill.id} skill={skill} />)}
+          {filteredSkills &&
+            filteredSkills.map((skill) => (
+              <SkillCard key={skill.id} skill={skill} />
+            ))}
         </div>
       </div>
     </div>

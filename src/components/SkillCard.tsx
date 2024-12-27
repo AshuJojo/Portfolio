8e4793a5c0b37f378ab7ee69ec521a936861e8e5
AshuJojo <ashutoshsharma.120599@gmail.com>
2024-12-27 22:25:02 +0530

    Fixed Image sizing issue in SkillCard.tsx:

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -9,17 +9,17 @@ interface SkillCardProps {
 const SkillCard = ({ skill }: SkillCardProps) => {
   return (
     <Card className="h-fit w-36 overflow-hidden border-2 border-black drop-shadow-2xl">
-      <div className="px-4 py-8">
+      <div className="px-4 py-4">
         <Image
-          className="m-auto"
+          className="m-auto w-auto h-32"
           src={
             typeof skill.icon === "string"
               ? (skill.icon ?? "/default-icon.png")
               : (skill.icon.url ?? "/default-icon.png")
           }
           alt={skill.skill}
-          width={64}
-          height={112}
+          width={24}
+          height={24}
         />
       </div>
       <CardHeader className="border-t-2 border-black bg-primary p-2">

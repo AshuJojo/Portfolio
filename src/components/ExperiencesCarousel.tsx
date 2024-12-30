f4eea465775e21e10bddd01cc78f9fcd3f7a7570
AshuJojo <ashutoshsharma.120599@gmail.com>
2024-12-30 12:04:34 +0530

    Added experiences.json mock data

--- src/components/ExperiencesCarousel.tsx
+++ src/components/ExperiencesCarousel.tsx
@@ -1,7 +1,16 @@
-const ExperiencesCarousel = () => {
-  return (
-    <div>ExperiencesCarousel</div>
-  )
-}
+"use client";
 
-export default ExperiencesCarousel
+import useFetch from "@/hooks/useFetch";
+import { Experience } from "@/payload-types";
+
+const ExperiencesCarousel = () => {
+  const {
+    data: experiences,
+    error: experiencesError,
+    loading: experiencesLoading,
+  } = useFetch<Experience>("/data/experiences.json");
+
+  return <div>ExperiencesCarousel</div>;
+};
+
+export default ExperiencesCarousel;

ac3755b470b89b42b0c58b8d35331b1e7fd4a70b
AshuJojo <ashutoshsharma.120599@gmail.com>
2024-12-31 11:11:02 +0530

    Updated ExperiencesCarousel with sorted experiences by date;

--- src/components/ExperiencesCarousel.tsx
+++ src/components/ExperiencesCarousel.tsx
@@ -2,17 +2,33 @@
 
 import useFetch from "@/hooks/useFetch";
 import { Experience } from "@/payload-types";
+import { useEffect, useState } from "react";
 import ExperienceCard from "./ExperienceCard";
 
 const ExperiencesCarousel = () => {
+  const [sortedExperiences, setSortedExperiences] = useState<Experience[]>([]);
   const {
     data: experiences,
     error: experiencesError,
     loading: experiencesLoading,
   } = useFetch<Experience>("/data/experiences.json");
 
-  return experiences?.length ? (
-    <ExperienceCard experience={experiences[0]} />
+  useEffect(() => {
+    if (experiences?.length) {
+      setSortedExperiences(
+        experiences.sort(
+          (a, b) =>
+            new Date(b.end_date ?? new Date()).getTime() -
+            new Date(a.start_date ?? new Date()).getTime(),
+        ),
+      );
+    }
+  }, [experiences]);
+
+  return sortedExperiences?.length ? (
+    sortedExperiences.map((experience) => (
+      <ExperienceCard key={experience.id} experience={experience} />
+    ))
   ) : (
     <></>
   );

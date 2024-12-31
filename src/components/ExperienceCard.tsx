be7f721eeb99713a6fe13ce00e80a81017071185
AshuJojo <ashutoshsharma.120599@gmail.com>
2024-12-31 12:59:16 +0530

    Updated ExperiencesCarousel.tsx with loading skeleton

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -70,6 +70,7 @@ const ExperienceCard = ({ experience }: ExperienceCardProps) => {
         )}
       </CardContent>
     </Card>
+    
   );
 };
 
--- src/components/ExperiencesCarousel.tsx
+++ src/components/ExperiencesCarousel.tsx
@@ -4,6 +4,7 @@ import useFetch from "@/hooks/useFetch";
 import { Experience } from "@/payload-types";
 import { useEffect, useState } from "react";
 import ExperienceCard from "./ExperienceCard";
+import { Card, CardContent } from "./ui/card";
 import {
   Carousel,
   CarouselContent,
@@ -11,6 +12,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "./ui/carousel";
+import { Skeleton } from "./ui/skeleton";
 
 const ExperiencesCarousel = () => {
   const [sortedExperiences, setSortedExperiences] = useState<Experience[]>([]);
@@ -45,7 +47,27 @@ const ExperiencesCarousel = () => {
       <CarouselNext />
     </Carousel>
   ) : (
-    <></>
+    <>
+      {experiencesLoading && (
+        <Card className="mx-auto mb-4 flex w-fit border-2 border-black p-8 drop-shadow-4xl">
+          {/* Card Media */}
+          <Skeleton className="h-24 w-24 rounded-full border-2 border-black p-2 shadow-4xl" />
+          <CardContent className="flex flex-col gap-6 pb-0">
+            <div className="flex flex-col gap-2 pb-0">
+              <Skeleton className="h-8 w-64 rounded-full" />
+              <Skeleton className="h-6 w-72 rounded-full" />
+              <Skeleton className="h-4 w-52 rounded-full" />
+            </div>
+            <div className="flex flex-col gap-2 pb-0">
+              <Skeleton className="h-4 w-96 rounded-full" />
+              <Skeleton className="h-4 w-96 rounded-full" />
+              <Skeleton className="h-4 w-96 rounded-full" />
+            </div>
+          </CardContent>
+        </Card>
+      )}
+      {experiencesError && <></>}
+    </>
   );
 };
 

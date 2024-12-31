0bc09c0f6c921decd13aecd205f29984965109e1
AshuJojo <ashutoshsharma.120599@gmail.com>
2024-12-31 11:03:10 +0530

    Updated ExperienceCard.tsx UI

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -1,31 +1,74 @@
 import { Experience } from "@/payload-types";
+import { RichText } from "@payloadcms/richtext-lexical/react";
+import { differenceInMonths } from "date-fns";
 import Image from "next/image";
-import { Card } from "./ui/card";
+import { Card, CardContent } from "./ui/card";
 
 interface ExperienceCardProps {
   experience: Experience;
 }
 
 const ExperienceCard = ({ experience }: ExperienceCardProps) => {
-  console.log("experience", experience);
+  const {
+    company_logo,
+    company_name,
+    role,
+    start_date,
+    end_date,
+    description,
+  } = experience;
+
+  const formatedStartDate: string = new Date(start_date).toLocaleDateString(
+    "en-US",
+    {
+      month: "short",
+      year: "numeric",
+    },
+  );
+
+  const formatedEndDate: string = end_date
+    ? new Date(end_date).toLocaleDateString("en-US", {
+        month: "short",
+        year: "numeric",
+      })
+    : "Present";
+
+  // Calculate the difference in months between the start and end date
+  const dateDifferenceInMonths =
+    differenceInMonths(end_date || new Date(), start_date) + 2;
+
   return (
-    <Card>
+    <Card className="max-w-1/2 flex w-fit border-2 border-black p-8 drop-shadow-4xl">
       {/* Card Media */}
-      <div className="shadow-4xl h-fit w-fit rounded-full border-2 border-black p-2">
+      <div className="h-fit w-fit rounded-full border-2 border-black p-2 shadow-4xl">
         <Image
-          className="h-20 w-20 object-contain"
+          className="h-16 w-16 object-contain"
           src={
-            experience.company_logo
-              ? typeof experience.company_logo === "string"
-                ? experience.company_logo
-                : experience.company_logo?.url || ""
+            company_logo
+              ? typeof company_logo === "string"
+                ? company_logo
+                : company_logo?.url || ""
               : ""
           }
-          alt={experience.company_name || "Company Logo"}
+          alt={company_name || "Company Logo"}
           width={0}
           height={0}
         />
       </div>
+      <CardContent className="pb-0">
+        <h3 className="text-2xl font-bold">{role}</h3>
+        <h4 className="text-lg font-semibold">{company_name}</h4>
+        <p className="text-sm font-semibold">
+          {formatedStartDate} - {formatedEndDate} • {dateDifferenceInMonths}{" "}
+          Months
+        </p>
+        {description && (
+          <RichText
+            data={description}
+            className="mt-2 [&>ul]:ml-5 [&>ul]:list-disc"
+          />
+        )}
+      </CardContent>
     </Card>
   );
 };
